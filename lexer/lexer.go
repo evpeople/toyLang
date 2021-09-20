bb8dddf5b0eec2dedde03eaabdbafbd462725726
evpeople <hangcaihui@gmail.com>
2021-09-20 17:22:11 +0800

    ✨ feat(lexer): new tokens
    new one-character token and keyword token

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -31,6 +31,21 @@ func (l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		tok = newToken(token.BANG, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
+		//运算符如上
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -39,15 +54,15 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.RPAREN, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+		//单目符号如上
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	//EOF如此
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
