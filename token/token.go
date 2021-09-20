337428a419cdde6bb9615fd9f04707ed77a13164
evpeople <hangcaihui@gmail.com>
2021-09-20 16:34:36 +0800

    ✨ feat(lexer): token struct

---
+++ token/token.go
@@ -0,0 +1,33 @@
+package token
+
+//TokenType string的别名
+type TokenType string
+
+//实际Token结构体
+type Token struct {
+	Type    TokenType
+	Literal string //原始数据
+}
+
+//目前的标识符等
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INI   = "INI"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
