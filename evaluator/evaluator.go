2e96d81f9e92f665be2ce4b2c85c5e08a61581f0
evpeople <hangcaihui@gmail.com>
2021-09-26 22:30:59 +0800

    ✨ feat(eva): finish everything

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -60,8 +60,71 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return &object.ReturnValue{Value: val}
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+	case *ast.CallExpression: //既调用ident，又调用func的ident
+		function := Eval(node.Function, env) //得到了对应的func，实际上是object.Func，也是ast.Ident ast.Func
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env) //先计算的参数列表中每个数的值
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
+
 	}
+
 	return nil
+}
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+	return unwrapReturnValue(evaluated)
+}
+func extendFunctionEnv(
+	fn *object.Function,
+	args []object.Object,
+) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+	return env
+}
+
+/*
+And this newly enclosed and updated environment is then the environment in which the function's
+body is evaluated. The result of this evaluation is unwrapped if it's an *object.ReturnValue.
+That's necessary, because otherwise a return statement would bubble up through several
+functions and stop the evaluation in all of them. But we only want to stop the evaluation of the last
+called function's body. That's why we need unwrap it, so that evalBlockStatement won't stop
+evaluating statements in "outer" functions.*/
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+	return obj
+}
+func evalExpressions(
+	exps []ast.Expression,
+	env *object.Environment,
+) []object.Object {
+	var result []object.Object
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
 }
 func evalIdentifier(
 	node *ast.Identifier,
