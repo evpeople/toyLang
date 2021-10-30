363a66675e5abd996709b40284307f335b1708b9
evpeople <hangcaihui@gmail.com>
2021-10-30 21:18:49 +0800

    🧪 test(parser): parser for Speak
    Speak 'hello' +'world'  && Speak 'hello world' 但是没有$name

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -50,3 +50,47 @@ func testStepStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 	return true
 }
+func TestSpeak(t *testing.T) {
+	input := `
+	Speak 'hello wo pao de zui kuai'
+	Speak 'hello'+' world'
+	`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParserProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 2 {
+		t.Fatalf("program.Statements does not contain 2 statements. got=%d",
+			len(program.Statements))
+	}
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"hello wo pao de zui kuai"},
+		{"hello world"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testSpeakStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+func testSpeakStatement(t *testing.T, s ast.Statement, sentence string) bool {
+	if s.TokenLiteral() != "Speak" {
+		t.Errorf("want 'Speak', got=%q", s.TokenLiteral())
+		return false
+	}
+	speakStmt, ok := s.(*ast.SpeakStatement)
+	if !ok {
+		t.Errorf("want *ast.SpeakStatement, got=%T", s)
+		return false
+	}
+	if speakStmt.Expression.TokenLiteral() != sentence {
+		t.Errorf("want %s, got=%s", sentence, speakStmt.Expression.TokenLiteral())
+		return false
+	}
+	return true
+}
