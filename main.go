29ba7e17e212e9e736e6a8b0e90b54ba34a679b0
evpeople <hangcaihui@gmail.com>
2021-11-12 22:03:16 +0800

    ✨ feat(main): first try main.go

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-	"evpeople/toyLang/bot"
-	"fmt"
+	"evpeople/toyLang/evaluator"
+	"evpeople/toyLang/lexer"
+	"evpeople/toyLang/object"
+	"evpeople/toyLang/parser"
 )
 
 func main() {
-	a := bot.New("sd", make(map[string]string), 21)
-	a.AddQa("ds", "wd")
-	fmt.Println(a)
 
+	input := `
+	Step welcome
+	Speak $name + ' happy'+'world'
+	Listen 2,3
+	Branch 'tousu',complainProc
+	Branch 'zhangdan',billProc
+	Silence silence
+	Default defaultProc
+	Step complainProc
+	Speak 'ni de yi jian shi wo men de'
+	Listen 2,4
+	Default thanks
+	`
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParserProgram()
+	env := object.NewEnvironment() //每次新建一个环境
+	env.Set("name", "evpeople")
+	gdf := parser.STEPINDEX
+	println(gdf)
+	// Eval(program, env).(*object.String).Value
+	evaluator.Eval(program, env)
+	// evEval2(program, env)
 }
