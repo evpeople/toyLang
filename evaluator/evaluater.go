25991302359c631098afdaa1e21857fc2e212327
evpeople <hangcaihui@gmail.com>
2021-11-27 22:36:54 +0800

    ✨ feat(eva): finish all sentence

--- evaluator/evaluater.go
+++ evaluator/evaluater.go
@@ -3,7 +3,12 @@ package evaluator
 import (
 	"evpeople/toyLang/ast"
 	"evpeople/toyLang/object"
+	"evpeople/toyLang/parser"
+	"fmt"
 	"strconv"
+	"strings"
+
+	// "strings"
 	"time"
 )
 
@@ -12,14 +17,12 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.Program:
 		// println("Program")
 		return evalProgram(node, env)
-		//TODO:从ast.StepStatement返回到这里，然后解包装，获取ident，然后从parser.STEPINDEX获取对应的数字。
-		//TODO：然后直接Eval(获取的下标对应的Statement数组的元素)
 	case *ast.SpeakStatement:
 		// println("Speak")
 		return evalSpeak(node, env)
 	case *ast.ListenStatement:
 		// println("Listen")
-		return evalListen(node, env) //TODO:返回一个字符串，进行一个时间延迟的channel，先sleep5s，然后wait最长时间，另一个goroutine里有键盘读取的代码，最后返回，读取到的信息。
+		return evalListen(node, env)
 		// case *ast.BranchStatement:
 		// case *ast.SilenceStatement:
 		// return evalBranch(node, env) //接受这个channel作为参数
@@ -27,7 +30,14 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		// println("Exit")
 		return evalExit(node, env)
 	case *ast.StepStatement:
-		return evalProgram(&ast.Program{Statements: node.ALLStatement}, env)
+		temp := evalProgram(&ast.Program{Statements: node.ALLStatement}, env)
+		_, ok := temp.(*object.String)
+		if ok {
+			temp.(*object.String).Value += "Step" //TODO:假如不能转换成功，说明是Exit//运行完了返回的
+			return temp
+		} else {
+			break
+		}
 		//TODO:从evalProgram 返回到这里，读取数据，然后再加一层包装，直接返回。
 	}
 	// println(node)
@@ -49,8 +59,27 @@ func evalListen(p *ast.ListenStatement, env *object.Environment) object.Object {
 	}
 	b, _ := strconv.Atoi(begin)
 	e, _ := strconv.Atoi(end)
-	time.Sleep(time.Duration(b+e) * 1 * time.Second)
+	time.Sleep(time.Duration(b) * 1 * time.Second)
+	c := make(chan string)
+	go func() {
+		fmt.Println("请输入答案")
+		var ans string
+		fmt.Scanln(&ans)
+		c <- ans
+	}()
+	select {
+	case m := <-c:
+		result.Value = "Listen" + m
+		handle(m)
+	case <-time.After(5 * time.Second * time.Duration(e)):
+		result.Value = "Listen" + "Silence"
+		handle("time out")
+	}
+	close(c)
 	return &result
+}
+func handle(m string) {
+	fmt.Println(m)
 }
 func evalSpeak(program *ast.SpeakStatement, env *object.Environment) object.Object {
 	var result object.String
@@ -71,29 +100,91 @@ func evalExit(program *ast.ExitStatement, env *object.Environment) object.Object
 	return &result
 }
 
+// func evalProgram(program *ast.Program, env *object.Environment) object.Object {
+// 	var result object.Object
+// 	for _, statement := range program.Statements {
+// 		result = Eval(statement, env)
+// 		temp := result.Inspect()
+// 		var temp2 string
+// 		if index := strings.Index(temp, "Listen"); index != -1 {
+// 			temp2 = temp[6:]
+// 			result.(*object.String).Value = temp2
+// 			return result
+// 		}
+
+// 		if index := strings.Index(temp, "Step"); index != -1 {
+// 			temp2 = temp[:index]
+// 			result.(*object.String).Value = temp2
+// 			// temp3 := statement.(*ast.StepStatement).CaseBranch[temp2]
+// 			return result
+// 		}
+
+// 		//TODO:result 的类型，如果为Listen的ans的话，添加到env中。
+// 		//TODO:在CaseBranch中，首先假设是Silence，然后在CaseBranch里读取对应的ident名字
+// 		//TODO:将ident包裹在Object中，直接在此return
+// 		//TODO: return
+
+// 		// switch result {
+// 		// case true:
+// 		// 	return true
+// 		// case *object.Error:
+// 		// 	return result //包装的值，处理错误的传递
+// 		// }
+// 		// result, ok := result.(*object.Boolean)
+// 		// if !ok {
+// 		// 	return result
+// 		// }
+// 		// if !result.Value {
+// 		// 	return result
+// 		// }
+// 	}
+// 	return result
+// }
+
 func evalProgram(program *ast.Program, env *object.Environment) object.Object {
 	var result object.Object
-	for _, statement := range program.Statements {
+	for i := 0; i < len(program.Statements); i++ {
+		statement := program.Statements[i]
 		result = Eval(statement, env)
+		temp := result.Inspect()
+		var temp2 string
+		if index := strings.Index(temp, "Listen"); index != -1 {
+			temp2 = temp[6:]
+			result.(*object.String).Value = temp2
+			return result
+		}
 
-		//TODO:result 的类型，如果为Listen的ans的话，添加到env中。
-		//TODO:在CaseBranch中，首先假设是Silence，然后在CaseBranch里读取对应的ident名字
-		//TODO:将ident包裹在Object中，直接在此return
-		//TODO: return
-
-		// switch result {
-		// case true:
-		// 	return true
-		// case *object.Error:
-		// 	return result //包装的值，处理错误的传递
-		// }
-		// result, ok := result.(*object.Boolean)
-		// if !ok {
-		// 	return result
-		// }
-		// if !result.Value {
-		// 	return result
-		// }
+		if index := strings.Index(temp, "Step"); index != -1 {
+			temp2 = temp[:index]
+			result.(*object.String).Value = temp2
+			temp2 = "tousu"
+			temp3 := statement.(*ast.StepStatement).CaseBranch[temp2]
+			temp4 := parser.STEPINDEX[temp3]
+			i = temp4 - 1
+			i = 4
+			// return result
+		}
 	}
+	// for _, statement := range program.Statements {
+
+	//TODO:result 的类型，如果为Listen的ans的话，添加到env中。
+	//TODO:在CaseBranch中，首先假设是Silence，然后在CaseBranch里读取对应的ident名字
+	//TODO:将ident包裹在Object中，直接在此return
+	//TODO: return
+
+	// switch result {
+	// case true:
+	// 	return true
+	// case *object.Error:
+	// 	return result //包装的值，处理错误的传递
+	// }
+	// result, ok := result.(*object.Boolean)
+	// if !ok {
+	// 	return result
+	// }
+	// if !result.Value {
+	// 	return result
+	// }
+	// }
 	return result
 }
